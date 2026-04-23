d37350ac14c0b48c2bdf5929dc5dd28498f4a922
Pedro Santana <pedro.costa.santana@gmail.com>
2026-04-23 15:47:56 +0100

    Refactoring: Replace instanceof cast with Pattern Matching in Position::equals

--- src/main/java/battleship/Position.java
+++ src/main/java/battleship/Position.java
@@ -215,8 +215,7 @@ public class Position implements IPosition {
 		if (this == otherPosition) {
 			return true;
 		}
-		if (otherPosition instanceof IPosition) {
-			IPosition other = (IPosition) otherPosition;
+		if (otherPosition instanceof IPosition other) {
 			return this.row == other.getRow() && this.column == other.getColumn();
 		}
 		return false;
